9706a04aa57fb9f9bec44efb0843ca84f5b3cf5c
rebeckahsiung <rebecka.hsiung@live.com>
2024-04-09 12:19:54 +0200

    random shit that comes up when trying to merge with main?

---
+++ Source/SPD_Spel1/RushEnemyAI.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RushEnemyAI.generated.h"
+
+UCLASS()
+class SPD_SPEL1_API ARushEnemyAI : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ARushEnemyAI();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+};
