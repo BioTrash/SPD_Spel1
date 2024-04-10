51fee345d76734bb81aa015bc3239b1cee68ed8d
Jeremy Vaudan <jeremyvaudan@hotmail.com>
2024-04-10 16:42:10 +0200

    fixed stuff

--- Source/SPD_Spel1/EnemyShooterAIController.cpp
+++ Source/SPD_Spel1/EnemyShooterAIController.cpp
@@ -60,8 +60,8 @@ void AEnemyShooterAIController::Tick(float DeltaSeconds)
                 if (HitResult.GetActor() == PlayerPawn)
                 {
                     DrawDebugLine(GetWorld(), StartTrace, EndTrace, FColor::Green, false, 0.1f, 0, 2);
-                    EnemyWeapon->PullTrigger(true);
-                    EnemyWeapon->PullTrigger(false);
+                    EnemyWeapon->PullTrigger();
+                    EnemyWeapon->PullTrigger();
                 }
             }
 
