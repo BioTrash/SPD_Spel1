d3a59667db7da961eda81517e74ccfb83b7a0498
Jeremy Vaudan <jeremyvaudan@hotmail.com>
2024-04-12 09:20:12 +0200

    updated aienemycontroller

--- Source/SPD_Spel1/EnemyShooterAIController.cpp
+++ Source/SPD_Spel1/EnemyShooterAIController.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "EnemyShooterAIController.h"
+
+#include "KismetTraceUtils.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "GameFramework/PlayerState.h"
 #include "Kismet/GameplayStatics.h"
@@ -24,6 +26,8 @@ void AEnemyShooterAIController::BeginPlay()
 
 void AEnemyShooterAIController::Tick(float DeltaSeconds)
 {
+    LastShotTime += DeltaSeconds;
+    
     APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
     if (PlayerPawn != nullptr)
     {
@@ -43,7 +47,8 @@ void AEnemyShooterAIController::Tick(float DeltaSeconds)
         if (EnemyWeapon)
         {
             EnemyWeapon->SetActorRotation(WeaponRotation);
-
+        if (LastShotTime >= ShootCooldown)
+        {
             // Vectors where trace is happening (Louis)
             FVector StartTrace = EnemyWeapon->GetActorLocation();
             FVector EndTrace = PlayerPawn->GetActorLocation();
@@ -57,14 +62,17 @@ void AEnemyShooterAIController::Tick(float DeltaSeconds)
             if (GetWorld()->LineTraceSingleByChannel(HitResult, StartTrace, EndTrace, ECC_GameTraceChannel1, CollisionParams))
             {
                 // If the ray hits the player, shoot (Louis)
-                if (HitResult.GetActor() == PlayerPawn)
+                if (HitResult.GetActor() == PlayerPawn && !HitResult.GetActor()->ActorHasTag("Enemy"))
                 {
-                    DrawDebugLine(GetWorld(), StartTrace, EndTrace, FColor::Green, false, 0.1f, 0, 2);
                     EnemyWeapon->PullTrigger(true);
-                    EnemyWeapon->PullTrigger(true);
+                    DrawDebugPoint(GetWorld(), HitResult.Location, 50, FColor::Green, true);
+                    EnemyWeapon->PullTrigger(false);
+
+                    //Resetta timern
+                    LastShotTime = 0.0f;
                 }
             }
-
+        }
             // Visualize the line trace
             
         }
