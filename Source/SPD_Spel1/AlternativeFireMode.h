79682db8d66d58118ed62d66d516cb8518dd5fba
rebeckahsiung <rebecka.hsiung@live.com>
2024-05-06 18:10:35 +0200

    SLIME WORKS (NOT MUZZLE FLASH)

---
+++ Source/SPD_Spel1/AlternativeFireMode.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NonProjectileWeapon.h"
+#include "AlternativeFireMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPD_SPEL1_API AAlternativeFireMode : public ANonProjectileWeapon
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void InitiateTimer(bool bButtonPressed, bool bAlternative) override;
+
+	void FireWeapon();
+
+	UFUNCTION(BlueprintPure)
+	int32 SetSlimeAmmo(int32 _SlimeAmmo);
+	
+	UFUNCTION(BlueprintPure)
+	int32 GetCurrentSlimeAmmo() const;
+	UFUNCTION(BlueprintPure)
+	FString GetSlimeAmmo() const;
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 SlimeAmmo;
+	UPROPERTY(BlueprintReadWrite)
+	int32 TotalSlimeAmmo;
+
+	UPROPERTY(EditAnywhere, Category="Weaponry")
+	TSubclassOf<class ASlimeProjectile> Projectile;
+	
+
+private:
+	bool IsWeaponCharging;
+	
+	float CurrentChargeTime;
+
+
+	
+};
