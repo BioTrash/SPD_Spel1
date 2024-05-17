9593bb40e6d4ba7d295ee1cfb7d42a3791717b2b
lawi22 <80677662+lawi22@users.noreply.github.com>
2024-05-17 10:48:48 +0200

    shooter boss minor setup & AI minor fix

--- Source/SPD_Spel1/ShooterBossAIController.cpp
+++ Source/SPD_Spel1/ShooterBossAIController.cpp
@@ -15,9 +15,6 @@ void AShooterBossAIController::BeginPlay()
 {
     Super::BeginPlay();
     
-    {
-        UE_LOG(LogTemp, Error, TEXT("Communication manager instance is nullptr."));
-    }
     InitiatePlayer();
     InitiateEnemy();
     if (AIBehavior != nullptr)
@@ -38,7 +35,7 @@ void AShooterBossAIController::Tick(float DeltaSeconds)
     {
         SetFocus(PlayerPawn);
     }
-    Enemy = Cast<AShooterEnemy>(GetPawn());
+    Enemy = Cast<AShooterBoss>(GetPawn());
     if (Enemy)
     {
         EnemyWeapon = Enemy->TriggerWeapon; //OBS! Ska bort. Enbart för att AI skapas innan Shooterenemy och därav är triggerweapon null i första beginplay().
@@ -49,87 +46,61 @@ void AShooterBossAIController::Tick(float DeltaSeconds)
 
         if (EnemyWeapon)
         {
+            UE_LOG(LogTemp, Warning, TEXT("Spamtimer: %f"), LastSpamShotTime);
+            //UE_LOG(LogTemp, Warning, TEXT("WEAPONGOOD"));
             SpamAttack();
 
             FVector PlayerLocation = PlayerPawn->GetActorLocation();
             EnemyLocation = Enemy->GetActorLocation();
             float DistanceToPlayer = FVector::Distance(EnemyLocation, PlayerLocation);
             
-                // Vectors where trace is happening (Louis)
-                FVector StartTrace = EnemyWeapon->GetActorLocation();
-                FVector EndTrace = PlayerPawn->GetActorLocation();
+            // Vectors where trace is happening (Louis)
+            FVector StartTrace = EnemyWeapon->GetActorLocation();
+            FVector EndTrace = PlayerPawn->GetActorLocation();
 
-                //Params for linetrace (Louis)
-                FHitResult HitResult;
-                FCollisionQueryParams CollisionParams;
-                CollisionParams.AddIgnoredActor(Enemy);
-                // Perform a line trace to check if there's a clear line of sight to the player (Louis)
-                if (GetWorld()->LineTraceSingleByChannel(HitResult, StartTrace, EndTrace, ECC_GameTraceChannel1, CollisionParams))
-                {
+            //Params for linetrace (Louis)
+            FHitResult HitResult;
+            FCollisionQueryParams CollisionParams;
+            CollisionParams.AddIgnoredActor(Enemy);
+            // Perform a line trace to check if there's a clear line of sight to the player (Louis)
+            if (GetWorld()->LineTraceSingleByChannel(HitResult, StartTrace, EndTrace, ECC_GameTraceChannel1, CollisionParams))
+            {
                     
-                    // If the ray hits the player, shoot (Louis)
-                    if (HitResult.GetActor() == PlayerPawn && !HitResult.GetActor()->ActorHasTag("Enemy"))
-                    {
-                        //Enemy updates position for PlayerLocation, LastKnownPlayerLocation, and signals it's shooting (Louis)
-                        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), HitResult.GetActor()->GetActorLocation());
+                // If the ray hits the player, shoot (Louis)
+                if (HitResult.GetActor() == PlayerPawn && !HitResult.GetActor()->ActorHasTag("Enemy"))
+                {
+                    //Enemy updates position for PlayerLocation, LastKnownPlayerLocation, and signals it's shooting (Louis)
+                    GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), HitResult.GetActor()->GetActorLocation());
 
-                        if (LastShotTime >= ShootCooldown)
+                    if (LastShotTime >= ShootCooldown)
+                    {
+                        if (ShootingEffect && !EffectIsPlaying)
                         {
-                            if (ShootingEffect && !EffectIsPlaying)
-                            {
-                                FVector SocketLocation = Enemy->GetStaticMeshComponent()->GetSocketLocation(TEXT("ProjectileSocket"));
-                                //UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ShootingEffect, EnemyWeapon->GetActorLocation()+100);
-                                NiagaraSystemComponent = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
-                                   GetWorld(),
-                                   ShootingEffect,
-                                   SocketLocation,
-                                   FRotator::ZeroRotator,
-                                   FVector::OneVector
-                               );
-                                EffectIsPlaying = true;
-                            }
-                            if (NiagaraSystemComponent)
-                            {
-                                NiagaraSystemComponent->SetWorldLocation(Enemy->GetActorLocation() + FVector(0, 0, 100)); 
-                            }
-                            const float EffectDuration = .9f;
-                            /*
-                            if (LastShotTime >= ShootCooldown + EffectDuration)
-                            {
-                                UE_LOG(LogTemp, Warning, TEXT("SHOOT!"));
-                                Shoot();
-                                Enemy->isShooting = true;
-                                LastShotTime = 0.0f;
-                                EffectIsPlaying = false;
-                            }*/
+                            FVector SocketLocation = Enemy->GetStaticMeshComponent()->GetSocketLocation(TEXT("ProjectileSocket"));
+                            //UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ShootingEffect, EnemyWeapon->GetActorLocation()+100);
+                            NiagaraSystemComponent = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+                               GetWorld(),
+                               ShootingEffect,
+                               SocketLocation,
+                               FRotator::ZeroRotator,
+                               FVector::OneVector
+                           );
+                            EffectIsPlaying = true;
+                        }
+                        if (NiagaraSystemComponent)
+                        {
+                            NiagaraSystemComponent->SetWorldLocation(Enemy->GetActorLocation() + FVector(0, 0, 100)); 
                         }
                     }
                     //Om Ray INTE Hit Player
-                        else
-                        {
-                            GetBlackboardComponent()->SetValueAsBool(TEXT("IsShooting"), false);
-                            Enemy->isShooting = false;
-                        }
+                    else
+                    {
+                        GetBlackboardComponent()->SetValueAsBool(TEXT("IsShooting"), false);
+                        Enemy->isShooting = false;
                     }
+                }
             }
         }
-}
-
-
-void AShooterBossAIController::Shoot()
-{
-    FVector SpawnLocation = EnemyWeapon->GetActorLocation(); //Byt ut till skeleton mesh senare
-    SpawnLocation.X -= 100;
-    FRotator SpawnRotation = WeaponRotation;
-    FActorSpawnParameters SpawnParams;
-    SpawnParams.Owner = this;
-    SpawnParams.Instigator = GetInstigator();
-
-    //Spawnar projectile och skjuter den med damage
-    AProjectile* Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, SpawnLocation+100, SpawnRotation, SpawnParams);
-    if (Projectile)
-    {
-        Projectile->SetDamage(15);
     }
 }
 
@@ -141,12 +112,32 @@ void AShooterBossAIController::SpamAttack()
         LastSpamShotTime = 0;
         SpamAmount++;
     }
+    if (SpamAmount <= 5)
+    {
+        SpamAmount = 0;
+    }
+}
+
+void AShooterBossAIController::Shoot()
+{
+    EnemyWeapon->ShootEnemyProjectile();
 }
 
 void AShooterBossAIController::InitiateEnemy()
 {
-    Enemy = Cast<AShooterEnemy>(GetPawn());
-    //EnemyWeapon = Enemy->TriggerWeapon;
+    Enemy = Cast<AShooterBoss>(GetPawn());
+    if (Enemy) // Ensure Enemy is not null
+    {
+        EnemyWeapon = Enemy->TriggerWeapon;
+        if (!EnemyWeapon)
+        {
+            UE_LOG(LogTemp, Error, TEXT("EnemyWeapon is null"));
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("Enemy is null"));
+    }
 }
 
 void AShooterBossAIController::InitiatePlayer()
